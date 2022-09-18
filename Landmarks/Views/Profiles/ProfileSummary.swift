66e6e5f86ec707504741388cc3585b97613fd506
Jose Rafael Guaura <jguaura@gmail.com>
2022-09-18 14:11:51 -0500

    feat: Hikes info in profile view

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+  @EnvironmentObject var modelData: ModelData
   var profile: Profile
   
   var body: some View {
@@ -13,6 +14,34 @@ struct ProfileSummary: View {
         Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
         Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
         Text("Goal Date: ") + Text(profile.goaldate, style: .date)
+        
+        Divider()
+        
+        VStack(alignment: .leading) {
+          Text("Completed Badges")
+            .font(.headline)
+          
+          ScrollView(.horizontal) {
+            HStack {
+              HikeBadge(name: "First Hike")
+              HikeBadge(name: "Earth Day")
+                .hueRotation(Angle(degrees: 90))
+              HikeBadge(name: "Tenth Hike")
+                .grayscale(0.5)
+                .hueRotation(Angle(degrees: 45))
+            }
+            .padding(.bottom)
+          }
+        }
+        
+        Divider()
+        
+        VStack(alignment: .leading) {
+          Text("Recent Hikes:")
+            .font(.headline)
+          
+          HikeView(hike: modelData.hikes[0])
+        }
       }
     }
   }
@@ -21,5 +50,6 @@ struct ProfileSummary: View {
 struct ProfileSummary_Previews: PreviewProvider {
   static var previews: some View {
     ProfileSummary(profile: Profile.default)
+      .environmentObject(ModelData())
   }
 }
